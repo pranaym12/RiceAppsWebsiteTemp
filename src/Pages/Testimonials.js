7d01af34b7b2268cf99b79c900f4f704abd4a907
Victor Song <vsong1618@gmail.com>
2018-10-13 16:01:13 -0500

    Created Testimonial page

---
+++ src/Pages/Testimonials.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './common.css';
+import './Testimonials.css';
+
+export default class Testimonials extends Component {
+    render() {
+        return (
+            <div className="TestimonialsWrapper">
+                <div className="outside-testimonials">
+                    <div className="horz-line"></div>
+                    <blockquote id="testament">
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                    </blockquote>
+                    <p id="testimonial-title">
+                        What people say about us
+                    </p>
+                </div>
+            </div>
+        )
+    }
+}

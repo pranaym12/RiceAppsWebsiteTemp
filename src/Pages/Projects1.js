cc79ed563402ca06842f8fd86881b9bae028d81f
jiinrhew <jr76@rice.edu>
2018-10-20 16:03:39 -0500

    JoinUs, Projects1, Testimonials with animation. Do npm i react-scroll-parallax --save

--- src/Pages/Projects1.js
+++ src/Pages/Projects1.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './common.css';
 import './Projects1.css';
+import { Parallax } from 'react-scroll-parallax';
 
 export default class Projects1 extends Component{
   render() {
     return(
       <div className="ProjectPageWrapper">
+      <Parallax offsetXMax={5} offsetXMin={-20}>
         <div className="projectstitle">
           <h1> We've delivered </h1>
           <hr/>
         </div>
+        </Parallax>
         <div className="projectSection">
           <div className="emptySpace"></div>
           <div className="emptySpace"></div>

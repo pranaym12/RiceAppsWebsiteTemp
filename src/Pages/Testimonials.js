c6651eefd6279a31f262095997395a4a1b33a9bc
Your Name <you@example.com>
2018-10-20 14:55:05 -0500

    change testimonials page to updated figma design

--- src/Pages/Testimonials.js
+++ src/Pages/Testimonials.js
@@ -7,13 +7,25 @@ export default class Testimonials extends Component {
         return (
             <div className="TestimonialsWrapper">
                 <div className="outside-testimonials">
-                    <div className="horz-line"></div>
-                    <blockquote id="testament">
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                    </blockquote>
-                    <p id="testimonial-title">
-                        Customers love us
-                    </p>
+                    <div id="testament">
+                      <p id = "quot1">
+                        “
+                      </p>
+                      <p id = "testText">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                      <p id = "quot2">
+                        ”
+                        </p>
+                    </div>
+                    <div id = "line1">
+                      <hr/>
+                    </div>
+                    <div id="testimonial-title">
+                        <p id = "titleText">
+                          What people say about us
+                        </p>
+                    </div>
                 </div>
             </div>
         )

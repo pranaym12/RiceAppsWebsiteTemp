95293ded5f7a43bc79c7ba886db0da17c913d412
Pranay Mittal <pranaym12@gmail.com>
2018-10-27 15:53:12 -0500

    fix another mege conflict

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -10,42 +10,6 @@ export default class LandingPage extends Component {
 
   render() {
 
-    var button_classes
-    var sidebar_menu_classes
-    var menu_div_class
-    if(this.state.clicked==true) {
-      button_classes = "hamburger hamburger--collapse is-active"
-      sidebar_menu_classes = "sidebar sidebarUnhidden"
-      menu_div_class = "blueMenu"
-    }
-    else{
-      button_classes = "hamburger hamburger--collapse "
-      sidebar_menu_classes = "sidebar sidebarHidden"
-      menu_div_class = ""
-    }
-    const menuLinkStyle = {
-      color:'#002FA4',
-
-      marginTop: '4vh'
-    }
-    return(
-      <div>
-        <div id="menuWrapper" class={menu_div_class}>
-          <button onClick={()=>this.handleClick()}
-              id="hamburger" class={button_classes} type="button">
-              <span class="hamburger-box">
-                <span class="hamburger-inner"></span>
-              </span>
-            </button>
-              <div id="sidebar" class={sidebar_menu_classes}>
-                <div class="sidebar-text">Home</div>
-                <div class="sidebar-text">Our Mission</div>
-                <div class="sidebar-text">Our Projects</div>
-                <div class="sidebar-text">Join Us</div>
-                <div class="sidebar-text">Contact sUs</div>
-              </div>
-
-        </div>
     
     return(
       <div>  

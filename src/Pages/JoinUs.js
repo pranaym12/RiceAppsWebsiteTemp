91e1756da1acca882ac7d06bbd63373cce51c8cf
Victor Song <vsong1618@gmail.com>
2018-11-03 14:49:34 -0500

    Added react-reveal animations instead of Parallax.

--- src/Pages/JoinUs.js
+++ src/Pages/JoinUs.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import './common.css';
 import './JoinUs.css';
-import { Parallax } from 'react-scroll-parallax';
+import Slide from 'react-reveal/Slide';
 
 export default class JoinUs extends Component {
     render() {
         return (
             <div className="JoinUsPageWrapper">
-            <Parallax offsetXMax={5} offsetXMin={-20}>
+            <Slide left>
               <div className="jointitle">
                 <h1> Do you have what it takes? </h1>
                 <hr/>
               </div>
-            </Parallax>
+            </Slide>
               <div className="text">
-                <Parallax offsetXMax={0} offsetXMin={11}><h1> Join us. </h1></Parallax>
-                <Parallax offsetXMax={-6} offsetXMin={16}><div> We’re looking for dedicated, passionate, and creative team members. </div></Parallax>
-                <Parallax offsetXMax={-16} offsetXMin={17}><div> Fill out this form, and we’ll be in touch with interview times and further details. </div></Parallax>
-                <Parallax offsetXMax={-28} offsetXMin={16}><div> Good luck! </div></Parallax>
+                <Slide right><h1> Join us. </h1></Slide>
+                <Slide right><div> We’re looking for dedicated, passionate, and creative team members. </div></Slide>
+                <Slide right><div> Fill out this form, and we’ll be in touch with interview times and further details. </div></Slide>
+                <Slide right><div> Good luck! </div></Slide>
               </div>
             </div>
         )
--- src/Pages/WorkForUs.js
+++ src/Pages/WorkForUs.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import './WorkForUs.css';
-import { Parallax } from 'react-scroll-parallax';
+import Slide from 'react-reveal/Slide'
 
 export default class JoinUs extends Component {
   render() {
     return (
       <div class = "content">
-      <Parallax offsetXMax={20} offsetXMin={-20}>
+      <Slide left>
         <div id = "LikeWhatYouSee">
             <hr/>
             <p id = "text1">Like what you see?</p>
         </div>
-      </Parallax>
+      </Slide>
 
-      <Parallax offsetXMax={-20} offsetXMin={20}>
+      <Slide right>
         <div id="WorkForUs">
           <p id = "text2"><a href="https://google.com">Work with Us.</a></p>
           <hr/>
         </div>
-      </Parallax>
+      </Slide>
 
       </div>
     )
